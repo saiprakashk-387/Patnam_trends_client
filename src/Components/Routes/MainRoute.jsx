86f8b6104ed1682409ca8a9c1dc3ef50f5b3a9a6
saiparakash <saiparakash@colanonline.com>
2022-07-11 18:44:41 +0530

    admin ui changes

--- src/Components/Routes/MainRoute.jsx
+++ src/Components/Routes/MainRoute.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import AdminDashboard from "../../Admin/AdminDashboard";
+import UserList from "../../Admin/UsersList";
 import Dashboard from "../../pages/Dashboard";
 import Login from "../../pages/Login";
 import Register from "../../pages/Register";
@@ -18,6 +19,7 @@ function MainRoute() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/myprofile" element={<MyProfile />} />
           <Route path ="/admindashboard" element={<AdminDashboard/>}/>
+          <Route path ="/adminuserlist" element={<UserList/>}/>
         </Route>       
       </Routes>
       
